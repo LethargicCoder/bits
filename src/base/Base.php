5ac26baa2778ca19ab18fade5b3125094c362b48
Eric Cestnick <eric.cestnick@gmail.com>
2024-11-30 17:59:06 -0400

    something committed

---
+++ src/base/Base.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Base class to build all from
+ */
+namespace LethargicCoder\Bits\Base;
+
+class Base 
+{
+    public static function test()
+    {
+        echo 'LethargicCoder\Bits\Base\Base->test()';
+    }
+}
---
+++ src/common/Link.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ *
+ */
+
+namespace LethargicCoder\Bits\Common;
+
+use LethargicCoder\Bits\Base\Base;
+
+class Link extends Base
+{
+
+}
